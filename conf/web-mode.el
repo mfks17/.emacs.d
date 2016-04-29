b09f850a2671bb3e584a9d0ccf9c4b853e9f1ccc
mfks17 <mfks17@gmail.com>
2016-04-29 09:35:45 +0900

    Update web-mode
    add JSX(React) syntax & Update configuration

--- conf/web-mode.el
+++ conf/web-mode.el
@@ -8,14 +8,17 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'"     . web-mode))
+
 
 ;;; インデント数
 (defun web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-html-offset   2)
+  (setq web-mode-attr-indent-offset nil)
+  (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-offset    2)
-  (setq web-mode-script-offset 2)
-  (setq web-mode-php-offset    2)
-  (setq web-mode-java-offset   2)
-  (setq web-mode-asp-offset    2))
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-sql-indent-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2))
 (add-hook 'web-mode-hook 'web-mode-hook)
